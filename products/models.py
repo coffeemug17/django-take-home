fbf7684b709e695040d2e3417d9a716478f94b31
coffeemug17 <mitulpandey2@gmail.com>
2026-03-17 19:55:53 -0700

    models for product, category, tag setup and migrations run

--- products/models.py
+++ products/models.py
@@ -1,3 +1,35 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        verbose_name_plural = 'Categories'
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+    
+class Product(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.TextField()
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.SET_NULL,
+        null=True,
+        related_name='products'
+    )
+    tags = models.ManyToManyField(
+        Tag,
+        blank=True,
+        related_name='products'
+    )
+
+    def __str__(self):
+        return self.name
