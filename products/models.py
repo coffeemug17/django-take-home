115459c4f3729176a65a01e7de25b0a2a37604ac
coffeemug17 <mitulpandey2@gmail.com>
2026-03-27 13:24:58 -0700

    Add database indexing and ordering to Category, Tag, and Product models

--- products/models.py
+++ products/models.py
@@ -1,22 +1,26 @@
 from django.db import models
 
 class Category(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=100, db_index=True)
 
     def __str__(self):
         return self.name
-    
+
     class Meta:
         verbose_name_plural = 'Categories'
+        ordering = ['name']
 
 class Tag(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=100, db_index=True)
 
     def __str__(self):
         return self.name
-    
+
+    class Meta:
+        ordering = ['name']
+
 class Product(models.Model):
-    name = models.CharField(max_length=200)
+    name = models.CharField(max_length=200, db_index=True)
     description = models.TextField()
     category = models.ForeignKey(
         Category,
@@ -32,3 +36,6 @@ class Product(models.Model):
 
     def __str__(self):
         return self.name
+
+    class Meta:
+        ordering = ['name']
